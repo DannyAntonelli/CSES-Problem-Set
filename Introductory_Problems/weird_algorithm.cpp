5431fc61a27435d06a9aafa0bbee4d88c5348045
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-04-05 08:35:36 +0200

    ✅ Weird Algorithm completed

---
+++ Introductory_Problems/weird_algorithm.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+
+    ll n;
+    cin >> n;
+
+    while (n > 1) {
+        cout << n << " ";
+        if (n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+    }
+    cout << 1;
+    return 0;
+}

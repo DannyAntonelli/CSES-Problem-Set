f710963a84b451a95490585b8c46c88afdacc1a1
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-04-06 19:39:04 +0200

    ✅ Bit String completed

---
+++ Introductory_Problems/bit_string.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+
+    const int MOD = 1000000007;
+    int n;
+    cin >> n;
+
+    ll res = 1;
+    FOR (_, 0, n) {
+        res *= 2;
+        res %= MOD;
+    }
+
+    cout << res;
+    
+    return 0;
+}

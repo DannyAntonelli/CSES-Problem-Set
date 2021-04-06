9b4e9469f1ead8a1eb6d3673dd430b886fe6d72a
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-04-06 20:41:51 +0200

    ✅ Trailing Zeros completed

---
+++ Introductory_Problems/trailing_zeros.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+
+    int n;
+    cin >> n;
+
+    int res = 0;
+    int div = 5;
+
+    while (div <= n) {
+        res += n / div;
+        n /= 5;
+    }
+
+    cout << res;
+
+    return 0;
+}

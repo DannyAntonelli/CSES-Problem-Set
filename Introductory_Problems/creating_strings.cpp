6c0db9632fadb831a760bbc94874cde3af53a98a
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-04-08 20:13:45 +0200

    ✅ Creating Strings completed

---
+++ Introductory_Problems/creating_strings.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+    set<string> set;
+
+    while (!set.count(s)) {
+        set.insert(s);
+        next_permutation(s.begin(), s.end());
+    }
+
+    cout << set.size() << "\n";
+    for (auto x : set) {
+        cout << x << "\n";
+    }    
+    return 0;
+}

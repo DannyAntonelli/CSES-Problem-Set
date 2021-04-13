621b6d4ea8352669cb742d6cfae18f6b04c35b15
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-04-13 15:02:50 +0200

    ✅ Static Range Sum Queries completed

---
+++ Range_Queries/static_range_sum_queries.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+
+    int n, q;
+    cin >> n >> q;
+    int x[n];
+    FOR (i, 0, n) cin >> x[i];
+
+    ll prefix[n];
+    ll sum = 0;
+    FOR (i, 0, n) {
+        sum += x[i];
+        prefix[i] = sum;
+    }
+
+    // FOR (i, 0, n) {
+    //     cout << prefix[i] << " ";
+    // } cout << "\n";
+
+    int a, b;
+    FOR (i, 0, q) {
+        cin >> a >> b;
+        cout << prefix[b-1] - prefix[a-1] + x[a-1] << "\n";
+    }
+    
+    return 0;
+}

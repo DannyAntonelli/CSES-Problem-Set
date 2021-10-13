04bf6e0211b9e85ca0dce3384a734379e38f0e3c
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-10-13 09:08:01 +0200

    ✅ Complete Exponentiation 2

---
+++ Mathematics/exponentiation_2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i > (b); --i)
+#define ALL(x) x.begin(), x.end()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+ll ipow(ll base, ll exp, ll mod) {
+    ll result = 1;
+    base %= mod;
+    for (; exp > 0; exp >>= 1) {
+        if (exp & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+    }
+    return result;
+}
+
+int main() {
+    fast_io();
+
+    const int MOD = 1e9 + 7;
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        ll exp = ipow(b, c, MOD - 1);
+        cout << ipow(a, exp, MOD) << "\n";
+    }
+
+    return 0;
+}
